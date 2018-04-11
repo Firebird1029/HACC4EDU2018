435c80a60b8a247cdf51602346d0969419b21c46
Brandon Yee <brandon.yee150@gmail.com>
2018-04-11 00:07:54 -1000

    Local Food Not Working

--- src/js/client.js
+++ src/js/client.js
@@ -4,3 +4,15 @@ var socket = io.connect();
 function searchYelp (term) {
 	socket.emit("searchYelp", {term: term});
 }
+
+function initMap () {
+	var uluru = {lat: -25.363, lng: 131.044};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
